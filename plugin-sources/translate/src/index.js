3a98c1aefb3363f8bf8ac59feae8a55d46fe1db5
Phil Nguyen <nquangphuong@gmail.com>
2022-03-04 12:54:34 +0700

    debug

--- plugin-sources/translate/src/index.js
+++ plugin-sources/translate/src/index.js
@@ -13,8 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
 
 /*
  * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
