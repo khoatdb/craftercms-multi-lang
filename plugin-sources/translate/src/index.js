75489c3428db9118b56db1fd128a5b1b02018eab
Phil Nguyen <nquangphuong@gmail.com>
2022-03-04 12:41:50 +0700

    update translate plugin

--- plugin-sources/translate/src/index.js
+++ plugin-sources/translate/src/index.js
@@ -16,34 +16,39 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+/*
+ * Copyright (C) 2007-2022 Crafter Software Corporation. All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import App from './App';
-import { render } from 'react-dom';
-import { createElement } from 'react';
 
-var plugin /*: PluginDescriptor */ = {
-  id: 'org.craftercms.translatePlugin',
-  name: 'Translate Plugin',
-  description: 'A plugin to copy selected items by copying them to an other locale location',
-  author: 'CrafterCMS',
+const plugin = {
+  id: 'org.pn.plugin.bulkedit',
+  name: 'Bulk Edit Plugin',
+  description: 'A plugin to do bulk edit',
+  author: 'Phil Nguyen',
   logo: null,
-  locales: {
-      en: en,
-      es: es
-  },
-  apps: [
-      {
-          route: '/yada-yada',
-          widget: { id: 'org.craftercms.translatePlugin.components.reactComponent' }
-      }
-  ],
+  locales: {},
+  apps: [],
   widgets: {
-      'org.craftercms.translatePlugin.components.reactComponent': App,
+    'org.pn.plugin.bulkedit.components.reactComponent': App,
   },
-  scripts: [
-  ],
+  scripts: [],
   stylesheets: [],
   themes: []
 };
 
-export default plugin;
 export { App };
+
+export default plugin;
